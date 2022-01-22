0788c55d27352138c451304114dc7dd2baed1076
DESKTOP-V359T0F\human <a>
2022-01-22 23:07:23 +0000

    v1 of workbook-payload.json and workbook-schema.json thought out. COBie 2.4 validation for sheets and unique keys (non-compound) is ok.

--- test_functions/schema_validation.py
+++ test_functions/schema_validation.py
@@ -1,18 +1,17 @@
 from jsonschema import Draft7Validator
 from json import load
-from test_functions.test_data_json.contact_records import contacts
 
 if __name__ == '__main__':
 
     # with open ('../schemas/definitions/contact') as f:
     #     schema = load(f)
 
-    with open ('../schemas/virtual/contact-extended') as f:
+    with open ('../schemas/definitions/workbook-schema.json') as f:
         schema = load(f)
 
     validator = Draft7Validator(schema)
 
-    with open('./test_data_json/contact_records') as p:
+    with open('test_data_json/workbook-payload.json') as p:
         payload = load(p)
 
     errors = validator.iter_errors(payload)
